581f23542840d154d615977bb5b9d9d9f3311639
Ed Lance <edlance@ntiertraining.com>
2026-09-02 17:44:45 -0400

    Remove all projects

--- Lab7.1/AttributeExampleOne/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿
-
-using AttributeExampleOne;
-
-ToolBox toolBox = new ToolBox();
-
-toolBox.OldMethod(); // This will generate a compiler warning due to the Obsolete attribute
-
-toolBox.NewMethod();
--- NuGetDemo/NuGetDemo/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿using ASimpleLogger;
-
-// instantiate the logger from the custom NuGet package
-Logger logger = new Logger();
-logger.LogMessage("Hello from the custom NuGet package demo!");
-
